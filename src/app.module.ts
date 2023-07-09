41129e127e848dd5fd0d4cd8020cae9a7fa3568a
hjpark625 <zero950@naver.com>
2023-07-09 19:56:39 +0900

    feat: mysql 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as mysql from 'mysql';
 import { LoggingMiddleware } from '@/middleware/logging.middleware';
 import type { MiddlewareConsumer } from '@nestjs/common';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: `${process.env.MYSQL_USERNAME}`,
+      password: `${process.env.MYSQL_PASSWORD}`,
+      database: `${process.env.MYSQL_DATABASE}`,
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      synchronize: true,
+      driver: mysql,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
